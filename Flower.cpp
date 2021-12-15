138dc32655da360f16d6f0c647eccf09eed10213
Yağız Can Aslan <yagizcanaslan0@gmail.com>
2021-12-15 09:07:59 +0300

    Removed some unnecessary comments

--- Flower.cpp
+++ Flower.cpp
@@ -6,27 +6,6 @@
 #include "Flower.h"
 using namespace std;
 
-/*
-public:
-        + Flower();
-        + Flower(string flowerName);
-        + Flower(const Flower& aFlower);
-        + ~Flower();
-        + bool isEmpty() const;
-        + int getLength() const ;
-        + bool add(string feature);
-        + bool remove(string feature);
-        + string printFlower() const;
-    private:
-        struct FeatureNode{
-            string feature;
-            FeatureNode* next;
-        };
-        int size;
-        string flowerName;
-        FeatureNode *head; // the features are stored in a sorted singly linear linked list
-*/
-
 Flower::Flower() {
     flowerName = "";
     size = 0;
