8f1c1a4c3cf14b8ecd5b6b627b140844fb669d06
Yağız Can Aslan <yagizcanaslan0@gmail.com>
2021-12-17 16:47:52 +0300

    Removed unnecessary comments

--- FlowerList.cpp
+++ FlowerList.cpp
@@ -5,28 +5,6 @@
 #include "FlowerList.h"
 using namespace std;
 
-/*
-class FlowerList {
-    public:
-        + FlowerList();
-        + FlowerList(const FlowerList& aList);
-        + ~FlowerList();
-        + bool isEmpty() const;
-        + int getLength() const;
-        bool retrieve(string flowerName, Flower& flower) const;
-        + bool add(string flowerName);
-        + bool remove(string flowerName);
-    private:
-        struct FlowerNode{
-            Flower f;
-            FlowerNode* next;
-        };
-        int size;
-        FlowerNode* head; // the flowers are stored in a sorted singly linear linked list
-        // ...
-        // you may define additional member functions and data members, if necessary
-};*/
-
 FlowerList::FlowerList() {
     size = 0;
     head = NULL;
